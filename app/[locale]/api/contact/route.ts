fd7bfdd27b0d621f2eaaeaa3c011db8b12de9266
idmjungle <idmjungle@gmail.com>
2025-06-27 21:26:53 -0700

    Change

--- app/[locale]/api/contact/route.ts
+++ app/[locale]/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { sendEmail } from "@/utils/mailer.utils";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const name = formData.get("name");
   const email = formData.get("email");
