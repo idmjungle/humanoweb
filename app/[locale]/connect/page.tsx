1578ad6c462ebdfac382a49a65b49ae98954f722
idmjungle <idmjungle@gmail.com>
2025-07-03 14:03:00 -0700

    Again

--- app/[locale]/connect/page.tsx
+++ app/[locale]/connect/page.tsx
@@ -1,7 +1,7 @@
 import ConnectClient from './client';
 import { generateLocalizedMetadata } from '@/lib/metadata';
 
-export async function generateMetadata({ params }: { params: { locale: string } }) {
+export async function generateMetadata({ params }) {
   const { locale } = params;
   return generateLocalizedMetadata(locale, 'ContactPage', '/connect');
 }
