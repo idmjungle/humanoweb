a6232a74b62ae6084eafde5296e0b7e1d846a3e4
idmjungle <idmjungle@gmail.com>
2025-07-03 23:10:32 -0700

    Final Fix?

--- app/[locale]/works/page.tsx
+++ app/[locale]/works/page.tsx
@@ -1,15 +1,13 @@
 import WorksClient from './client';
 import { generateLocalizedMetadata } from '@/lib/metadata';
 
-interface GenerateMetadataParams {
-  params: {
-    locale: string;
-  };
-}
-
-export async function generateMetadata({ params }: Promise<GenerateMetadataParams>) {
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
   const { locale } = await params;
-  return generateLocalizedMetadata(locale, 'WorksPage', '/works');
+  return generateLocalizedMetadata(locale, "WorksPage", "/works");
 }
 
 export default function WorksPage() {
